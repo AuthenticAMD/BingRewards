fad9fcfbe1a4ba8e66f94138a087b7588e55554c
AuthenticAMD <amdpowered@gmail.com>
2014-11-19 18:07:09 -0500

    Update googleTrends.py
    Re-factored code to support Python 3

--- pkg/queryGenerators/googleTrends.py
+++ pkg/queryGenerators/googleTrends.py
@@ -1,93 +1,70 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 #
-# Google Trends queries generator
-# Developed by Sangdrax (2014)
+# Wikipedia queries generator
+# developed by Alex Mayer (2014)
 #
 
-# This generatoer generates whole words, generally consistent 
-# with a search syntax.  These terms are anything trending
-# and can be NSFW or terms for illegal items.
-
+import re
+import helpers
 import urllib
-import random
-from xml.etree import ElementTree
+from random import randint
+from datetime import date
+from bingRewards import BingRewards
 
-TRENDSURL = "http://www.google.com/trends/hottrends/atom/feed?pn=p1"
-SUGGESTURL = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q="
-MAX_QUERY_LEN = 50
+month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+QUERY_URL = "http://en.wikipedia.com/wiki/{0}_{1}".format(month_names[date.today().month - 1], date.today().strftime("%d"))
 
 class queryGenerator:
-
     def __init__(self, br):
-        self.br = br
-        self.allQueries = self.__pullAll()
-        self.unusedQueries = self.allQueries.copy()
-
-    def __pullAll(self):
-        result = self.__trendQueries()
-        tmp = result.copy()
-        for term in tmp:
-            suggestSet = self.__suggestQueriesSingle(term)
-            result.update(suggestSet)
-        return result.copy()
-    
-    def __readXML(self,URL):
-        response = urllib.request.urlopen(URL)
-        try:
-            tree = ElementTree.parse(response)
-        except:
-            return None
-        return tree
-
-    def __trendQueries(self):
-        generated = set()
-        tree = self.__readXML(TRENDSURL)
-        if tree is None: raise TypeError("trend could not parse XML")
-
-        for item in tree.iter("item"):
-            title = item.find('title').text
-            desc = item.find('description').text
-            generated.add(title.strip().lower())
-            if desc:
-                subDesc = desc.split(',')
-                subDesc = subDesc if len(subDesc) <= 10 else random.sample(subDesc, 10)
-                for item in subDesc:
-                    generated.add(item.strip().lower())
-        return generated
-
-    def __suggestQueriesSingle(self,term):
-        suggestions = set()
-        formatted = urllib.parse.quote_plus(term.encode('utf-8')) #term.replace(" ","+").encode('ascii', 'ignore')
-        URL = SUGGESTURL+formatted
-        tree = self.__readXML(URL)
-        if tree is not None: 
-            for item in tree.iter("suggestion"):
-                suggestions.add(item.get('data').lower())
-        return suggestions
-
-    def generateQueries(self, queriesToGenerate, history, maxQueryLen = MAX_QUERY_LEN):
         """
-        parses Google Trends top trends and generates a set of unique queries. If the number
-        of queries is not >= the requested number, a google suggestion will be called on all
-        terms in the query set.  These suggestions will be added to the set as well.
+        param br is a pointer to the calling class bingRewards (used for variables)
+        """
+        if br is None or not isinstance(br, BingRewards):
+            raise ValueError("br is not set or is not an instance of BingRewards")
+        self.bingRewards = br
+
+    def generateQueries(self, queriesToGenerate, history, maxQueryLen = None):
+        """
+        Parses the current days wikipedia.com page and generates queries
+        from the links on the page.
 
         param queriesToGenerate the number of queries to return
         param history a set of previous searches
+        param maxQueryLen the maximum query length
+
+        returns a set of queries - self.queries
         """
-        if queriesToGenerate <= 0: raise ValueError("queriesToGenerate should be more than 0, but it is %d" % queriesToGenerate)
-        if history is None or not isinstance(history, set): raise ValueError("history is not set or not an instance of set")
+        if queriesToGenerate <= 0:
+            raise ValueError("numberOfQueries should be more than 0, but it is %d" % queriesToGenerate)
+        if history is None or not isinstance(history, set):
+            raise ValueError("history is not set or not an instance of set")
 
-        removedHistory = self.unusedQueries.intersection(history)
-        self.unusedQueries -= history
-        if queriesToGenerate > len(self.unusedQueries):
-            self.unusedQueries = self.__pullAll()
-            if queriesToGenerate > len(self.unusedQueries): raise ValueError("to many queries requested")
+        request = urllib.request.Request(url = QUERY_URL, headers = self.bingRewards.httpHeaders)
+        with self.bingRewards.opener.open(request) as response:
+            page = helpers.getResponseBody(response).decode('utf-8')
 
-        final = random.sample(self.unusedQueries, queriesToGenerate)
-        finalSet = set(final)
-        self.unusedQueries -= finalSet
-        self.unusedQueries.update(removedHistory)
-        return finalSet.copy()
+        if page.strip() == "": raise ValueError("wikipedia page is empty")
 
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+        # get rid of new lines
+        page = page.replace("\n", "")
+        # seperate out the guts of the article
+        page = re.search('id="Events"(.+?)id="External_links"', page, re.I)
+        if page == None:
+            raise ValueError("wiki page has no contents")
+        # seperate out each result
+        searchTerms = re.findall(r'<li.+?<a href="/wiki/.+?".*?>([a-zA-Z\s]+?)</a>.*?</li>', page.group(1))
+
+        queries = set()
+        queriesNeeded = queriesToGenerate
+        while queriesNeeded > 0 and len(searchTerms) > 0:
+            ri = randint(0, len(searchTerms) - 1)
+            # ignore things in your history
+            if searchTerms[ri] in history:
+                del searchTerms[ri]
+                continue
+            queries.add(searchTerms[ri])
+            del searchTerms[ri]
+            queriesNeeded -= 1
+
+        return queries
